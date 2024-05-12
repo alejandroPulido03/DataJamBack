f065e4172d49d251160937afe4cc915ff7f365e3
Pandlop <jc.lopezc1@uniandes.edu.co>
2024-05-12 13:29:20 -0500

    a ver

--- benchmark_tool_v2/companies/views.py
+++ benchmark_tool_v2/companies/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@csrf_exempt
+# @csrf_exempt
 @api_view(["POST"])
 def loginCompany(request):
     if request.method == "POST":
