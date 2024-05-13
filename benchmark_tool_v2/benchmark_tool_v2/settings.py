6b43039660287da09a7c5500a93577c5bd0f04ea
alejandroP03 <d.pulidob@uniandes.edu.co>
2024-05-12 23:10:08 -0500

    employee email logic

--- benchmark_tool_v2/benchmark_tool_v2/settings.py
+++ benchmark_tool_v2/benchmark_tool_v2/settings.py
@@ -138,3 +138,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'datajam.walkfree@gmail.com'
+EMAIL_HOST_PASSWORD = 'gxzf diql qzgh pekb'
--- benchmark_tool_v2/companies/logic/employee_email_logic.py
+++ benchmark_tool_v2/companies/logic/employee_email_logic.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from ..models import EmployeeEmail
 from rest_framework.validators import UniqueValidator
+from ...benchmark_tool_v2.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+
 
 class EmployeeEmailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,6 +14,15 @@ class EmployeeEmailSerializer(serializers.ModelSerializer):
         ]
 
 
+def send_emails(company_pk):
+    employees = EmployeeEmail.objects.filter(company_id=company_pk)
+    for employee in employees:
+        subject = 'Walk Free Survey'
+        message = '''
+        Hello, you have been invited to take a survey in order to measure your company\'s performance in terms of human rights and fight against modern
+        slavery. Please click on the following link to start the anonymous survey: http://localhost:4200/make_anonymous_survey/''' + str(company_pk) + '''/
+        '''
+        receiver = employee.email
+        send_mail(subject, message, EMAIL_HOST_USER, [receiver])
 
-
-# TODO send emails
+    
