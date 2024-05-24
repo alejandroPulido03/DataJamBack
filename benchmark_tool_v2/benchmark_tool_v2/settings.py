9a913a99b2d91638efaeba55ba06b7837a60880c
Alejandro Pulido <110847237+alejandroP03@users.noreply.github.com>
2024-05-23 23:51:40 -0500

    Update env variables calls

--- benchmark_tool_v2/benchmark_tool_v2/settings.py
+++ benchmark_tool_v2/benchmark_tool_v2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,12 +88,10 @@ WSGI_APPLICATION = "benchmark_tool_v2.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "bench_tool_v2_db",
-        "USER": "bench_tool_v2",
-        "PASSWORD": "discordinados",
-        # "HOST": "172.17.0.2",
-        "HOST": "34.171.154.238",
-        # "PORT": "5432",
+        "NAME": os.getenv("DB_NAME")
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST")
     }
 }
 
@@ -143,5 +141,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'datajam.walkfree@gmail.com'
-EMAIL_HOST_PASSWORD = 'gxzf diql qzgh pekb'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
